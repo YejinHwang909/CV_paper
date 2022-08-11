13448dc1e4f45f8ca641d429e9454a00a7b9d63d
Yejin <ghkd970909@naver.com>
2022-08-11 23:49:56 +0900

    Change detr

--- DETR/detr.py
+++ DETR/detr.py
@@ -13,3 +13,17 @@ from .segmentation import (DETRsegm, PostProcessPanoptic, PostProcessSegm,
                             dice_loss, sigmoid_focal_loss)
 from .transformer import build_transformer
 
+class DETR(nn.Module):
+    def __init__(self, backbone, transformer, num_classes, num_queries, aux_loss=False):
+        super().init__()
+        self.num_queries = num_queries
+        self.transformer = transformer
+        hidden_dim = transformer.d_model
+        self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
+        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
+        self.query_embed = nn.Embedding(num_queries, hidden_dim)
+        self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
+        self.backbone = backbone
+        self.aux_loss = aux_loss
+
+        def forward(self, samples: NestedTensor):
